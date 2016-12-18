9022797bb96a4a2d92cf3ac0be5479f2afc7284c
jemm.tang <261892339@qq.com>
2016-12-18 17:55:00 +0800

    add post-detail page

--- demo/pages/posts/post-detail/post-detail.js
+++ demo/pages/posts/post-detail/post-detail.js
@@ -3,6 +3,7 @@ Page({
   data:{},
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    console.log(options.id)
   },
   onReady:function(){
     // 页面渲染完成
--- demo/pages/posts/post.js
+++ demo/pages/posts/post.js
@@ -7,5 +7,12 @@ Page({
   },
   onLoad() {
     this.data.postsList = postsList;
+  },
+  gotoDetailPage: (event)=>{
+    var postId = event.currentTarget.dataset.postid;
+    
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id='+postId
+    })
   }
 })
